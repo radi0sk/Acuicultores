a94a66deb6afd8bb3678a8b82fd61e78b81bb46d
pedro Ramirez <pedrora3@gmail.com>
2025-09-13 22:43:01 +0000

    Try fixing this error: `Console Error: `DialogContent` requires a `Dialo

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger, DropdownMenuGroup, DropdownMenuRadioGroup, DropdownMenuRadioItem } from '@/components/ui/dropdown-menu';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuGroup, DropdownMenuRadioGroup, DropdownMenuRadioItem } from '@/components/ui/dropdown-menu';
 import { LayoutDashboard, Briefcase, MessagesSquare, BookOpen, Newspaper, LogOut, Check, ShoppingCart, Menu, Heart, ThumbsUp, ShieldCheck, Home } from 'lucide-react';
 import React from 'react';
 import PublicNav from '@/components/PublicNav';
@@ -120,6 +120,9 @@ function AppLayoutContent({ children }: { children: React.ReactNode }) {
                           </Button>
                       </SheetTrigger>
                       <SheetContent side="left" className="sm:max-w-xs">
+                          <SheetHeader className="hidden">
+                            <SheetTitle>Menú de Navegación</SheetTitle>
+                          </SheetHeader>
                           <div className="p-6">
                               <DashboardNav />
                           </div>
@@ -176,6 +179,9 @@ function AppLayoutContent({ children }: { children: React.ReactNode }) {
                       </Button>
                   </SheetTrigger>
                   <SheetContent side="left" className="sm:max-w-xs">
+                      <SheetHeader className="hidden">
+                        <SheetTitle>Menú de Navegación</SheetTitle>
+                      </SheetHeader>
                       <div className="p-6">
                            <DashboardNav />
                       </div>
